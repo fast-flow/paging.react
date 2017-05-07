452f14163adfb8631587e2947eb6e22e4aba8a74
nimojs <nimo.jser@gmail.com>
2017-05-07 15:49:23 +0800

    lang example

--- example/README.md
+++ example/README.md
@@ -8,9 +8,31 @@
 
 ````demo
 {
+    title: 'onChange',
+    html: '<div id="example__onChange" >loading...</div>',
+    desc: '`page={2} pageCount={10} pageSize={10} onChange={fn}`',
+    file: './onChange.demo.js'
+}
+````
+
+## props.dataCount
+
+````demo
+{
+    title: 'dataCount',
+    html: '<div id="example__dataCount" >loading...</div>',
+    desc: '`page={2} dataCount={232} pageSize={10} onChange={fn}`',
+    file: './dataCount.demo.js'
+}
+````
+
+## props.lang
+
+````demo
+{
     title: 'basic',
-    html: '<div id="example__basic" >loading...</div>',
-    desc: '`<Paging page={2} pageCount={10} pageSize={10}  />`',
-    file: './basic.demo.js'
+    html: '<div id="example__lang" >loading...</div>',
+    desc: '`Use props.lang change text`',
+    file: './lang.demo.js'
 }
 ````
--- example/dev.js
+++ example/dev.js
@@ -1,14 +1,32 @@
 var ReactDOM = require('react-dom')
 var render = ReactDOM.render
 
-// basic
+// onChange
 ;(function (node) {
     if (!node) {return}
-    require(['./basic.demo.js'], function (Demo) {
+    require(['./onChange.demo.js'], function (Demo) {
         Demo = Demo.default || Demo
         render(<Demo />, node)
     })
-})(document.getElementById('example__basic'))
+})(document.getElementById('example__onChange'))
+
+// lang
+;(function (node) {
+    if (!node) {return}
+    require(['./lang.demo.js'], function (Demo) {
+        Demo = Demo.default || Demo
+        render(<Demo />, node)
+    })
+})(document.getElementById('example__lang'))
+
+// dataCount
+;(function (node) {
+    if (!node) {return}
+    require(['./dataCount.demo.js'], function (Demo) {
+        Demo = Demo.default || Demo
+        render(<Demo />, node)
+    })
+})(document.getElementById('example__dataCount'))
 
 // custom-style
 ;(function (node) {
